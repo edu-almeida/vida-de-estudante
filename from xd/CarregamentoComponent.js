c978e8adf4756e424aedea4f78510751e3e4b792
Eduardo Almeida <60547531+edu-almeida@users.noreply.github.com>
2022-03-30 19:27:50 -0300

    componetnes from xd

---
+++ from xd/CarregamentoComponent.js
@@ -0,0 +1,120 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import Svg, {
+  Defs,
+  ClipPath,
+  Rect,
+  G,
+  Circle,
+  Ellipse,
+  Text,
+  TSpan,
+  Path,
+  Image
+} from "react-native-svg";
+/* Adobe XD React Exporter has dropped some elements not supported by react-native-svg: style */
+
+const CarregamentoComponent = () => (
+  <Svg width={412} height={778} viewBox="0 0 412 778">
+    <Defs>
+      <ClipPath id="b">
+        <Rect width={412} height={778} />
+      </ClipPath>
+    </Defs>
+    <G id="a" className="a">
+      <Rect className="f" width={412} height={778} />
+      <G transform="translate(0 -523.778)">
+        <G className="b" transform="translate(-169 927.778)">
+          <Circle className="h" cx={218} cy={218} r={218} />
+          <Circle className="i" cx={218} cy={218} r={217.5} />
+        </G>
+        <G className="b" transform="translate(159 570.778)">
+          <Circle className="h" cx={218} cy={218} r={218} />
+          <Circle className="i" cx={218} cy={218} r={217.5} />
+        </G>
+        <G className="b" transform="translate(238 912.778)">
+          <Ellipse className="h" cx={164.5} cy={148.5} rx={164.5} ry={148.5} />
+          <Ellipse className="i" cx={164.5} cy={148.5} rx={164} ry={148} />
+        </G>
+        <G className="b" transform="translate(-62 1091.778)">
+          <Ellipse className="h" cx={164.5} cy={148.5} rx={164.5} ry={148.5} />
+          <Ellipse className="i" cx={164.5} cy={148.5} rx={164} ry={148} />
+        </G>
+      </G>
+      <Text className="c" transform="translate(138 732)">
+        <TSpan x={0} y={0}>
+          {"Carregando carteira..."}
+        </TSpan>
+      </Text>
+      <G transform="translate(180.625 661.75)">
+        <Path
+          className="d"
+          d="M27.919,9.46a7.21,7.21,0,1,1-7.21-7.21A7.21,7.21,0,0,1,27.919,9.46Z"
+          transform="translate(6.097 0)"
+        />
+        <Path
+          className="d"
+          d="M14.189,20.032a5.407,5.407,0,1,1-5.407-5.407A5.407,5.407,0,0,1,14.189,20.032Z"
+          transform="translate(0 7.451)"
+        />
+        <Path
+          className="d"
+          d="M27.115,10.45a.9.9,0,1,1-.9-.864.9.9,0,0,1,.9.864Z"
+          transform="translate(13.21 4.417)"
+        />
+        <Path
+          className="d"
+          d="M16.348,8.2a5.707,5.707,0,0,0-8.123,0,5.842,5.842,0,0,0,0,8.2,5.707,5.707,0,0,0,8.123,0A5.842,5.842,0,0,0,16.348,8.2Z"
+          transform="translate(1.908 2.563)"
+        />
+        <Path
+          className="d"
+          d="M31.73,18.677a1.8,1.8,0,1,1-1.8-1.8A1.8,1.8,0,0,1,31.73,18.677Z"
+          transform="translate(14.903 8.806)"
+        />
+        <Path
+          className="d"
+          d="M29.665,26.937a2.7,2.7,0,1,1-2.7-2.639,2.7,2.7,0,0,1,2.7,2.639Z"
+          transform="translate(12.574 13.276)"
+        />
+        <Path
+          className="d"
+          d="M22.96,30.6a3.6,3.6,0,1,1-3.6-3.6A3.6,3.6,0,0,1,22.96,30.6Z"
+          transform="translate(7.451 14.903)"
+        />
+        <Path
+          className="d"
+          d="M16.325,27.569a4.506,4.506,0,1,1-4.506-4.507,4.507,4.507,0,0,1,4.506,4.507Z"
+          transform="translate(2.371 12.532)"
+        />
+      </G>
+      <G transform="translate(0 -1)">
+        <Text className="e" transform="translate(80 469)">
+          <TSpan x={0} y={0}>
+            {"Vida de estudante"}
+          </TSpan>
+        </Text>
+        <Image
+          width={148}
+          height={125}
+          transform="translate(132 301)"
+          xlinkHref="ComponentTMP_0-image.png"
+        />
+      </G>
+      <Rect className="f" width={412} height={27} />
+      <Path
+        className="g"
+        d="M39.174,0H53L46.087,11ZM26.24,11a5.5,5.5,0,0,1,0-11h.521a5.5,5.5,0,1,1,0,11ZM0,11V0H11.522V11Z"
+        transform="translate(350 8)"
+      />
+    </G>
+  </Svg>
+);
+
+export default CarregamentoComponent;
